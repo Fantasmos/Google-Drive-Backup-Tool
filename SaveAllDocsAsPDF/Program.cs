49780181c6dacae0cddfb57557a1ccca0ac906e0
Fantasmos <benji.k@live.com.au>
2017-11-05 20:36:11 +1100

    Refactoring, better handling of different file formats

--- SaveAllDocsAsPDF/Program.cs
+++ SaveAllDocsAsPDF/Program.cs
@@ -6,146 +6,30 @@ using Google.Apis.Util.Store;
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace DriveQuickstart
 {
-
-
-   
-    class Program
+    internal class Program
     {
+        private static string ApplicationName = "Drive API .NET Quickstart";
 
-        static void ResaveFile (DriveService service, Google.Apis.Drive.v3.Data.File file, Google.Apis.Drive.v3.Data.File fileMetadata, string metadatatype = "application/pdf")
-        {                        //Create New File
-            var stream = new System.IO.MemoryStream();
-            service.Files.Export(file.Id, metadatatype).Download(stream);
-            using (stream)
-            {
-                FilesResource.CreateMediaUpload request;
-                request = service.Files.Create(fileMetadata, stream, metadatatype);
+        private static bool RanOnce = false;
 
-                request.Fields = "id";
-                request.Upload();
-                var item = request.ResponseBody;
-            }
-        }
         // If modifying these scopes, delete your previously saved credentials
         // at ~/.credentials/drive-dotnet-quickstart.json
-        static string[] Scopes = { DriveService.Scope.Drive };
-        static string ApplicationName = "Drive API .NET Quickstart";
-        static bool RanOnce = false;
-        static string SubfolderName = "ResavedPDFs";
+        private static string[] Scopes = { DriveService.Scope.Drive };
 
-        static string GetOrCreateFolder (DriveService service, string SuperFolder)
+        private static string SubfolderName = "ResavedPDFs";
+
+        struct format
         {
-            Google.Apis.Drive.v3.Data.File SubFolder = GetFolderInSubfolder(service, SuperFolder);
-
-
-            if (SubFolder?.Id != null)
-            {
-                return SubFolder.Id;
-            }
-            else
-            {
-                var FolderMetadata = new Google.Apis.Drive.v3.Data.File()
-                {
-                    Name = SubfolderName,
-                    MimeType = "application/vnd.google-apps.folder"
-                };
-                if (string.IsNullOrEmpty(SuperFolder))
-                {
-                    //do nothing
-                }
-                else
-                {
-                    FolderMetadata.Parents = new List<string>();
-                    FolderMetadata.Parents.Add(SuperFolder);
-                }
-                var request = service.Files.Create(FolderMetadata);
-                request.Fields = "id";
-
-                var folder = request.Execute();
-                Console.WriteLine("Folder ID: " + folder.Id);
-
-                return folder.Id;
-            }
-        }
-
-        static Google.Apis.Drive.v3.Data.File GetFolderInSubfolder(DriveService driveService , string parent)
-        {
-            try
-            {
-                string pageToken = null;
-                do
-                {
-                    var request = driveService.Files.List();
-                    request.Q = "mimeType='application/vnd.google-apps.folder'";
-                    request.Spaces = "drive";
-                    request.Fields = "nextPageToken, files(id, name, parents)";
-                    request.PageToken = pageToken;
-                    var result = request.Execute();
-                    foreach (var file in result.Files)
-                    {
-                        if (string.IsNullOrEmpty(parent) | file?.Parents == null)
-                        {
-                            if (file.Name.Equals(SubfolderName))
-                            {
-                                return file;
-                            }
-
-                        }
-                        else
-                        {
-                            if (file.Name.Equals(SubfolderName) && file.Parents.Contains(parent))
-                            {
-                                return file;
-                            }
-
-                        }
-                    }
-                    pageToken = result.NextPageToken;
-                } while (pageToken != null);
-
-                return null;
-            }
-            catch
-            {
-                Console.WriteLine("Threw error");
-                return null;
-            }
-        }
-
-
-        static void Main(string[] args)
-        {
-           
-
-            bool WantToKeepRunning = true;
-            if (args.Length > 0)
-            {
-                WantToKeepRunning = bool.Parse(args[0]);
-            }
-            int minutes = 5;
-            if (args.Length > 1)
-            {
-                minutes = int.Parse(args[1]);
-            }
-
-            Console.WriteLine("Timer set to: " + minutes);
+            public string name;
+            public string MimeHeader;
             
-            do
-            {
-                CheckAndUpdate();
-                Thread.Sleep(minutes * 1000 * 60);
-            } while (WantToKeepRunning | (RanOnce == false));
         }
-
-        static bool CheckAndUpdate()
+        private static bool CheckAndUpdate()
         {
             UserCredential credential;
             Console.WriteLine("Started running at: " + DateTime.Now.TimeOfDay);
@@ -192,9 +76,7 @@ namespace DriveQuickstart
             catch (Exception ex)
             {
                 Console.WriteLine("When this program attempted to read/load {0} an error occured! The program will not filter files that have not been changed.", LastModifiedUnixDate);
-                
             }
-            
 
             // Create Drive API service.
             var service = new DriveService(new BaseClientService.Initializer()
@@ -206,14 +88,13 @@ namespace DriveQuickstart
             // Define parameters of request.
 
             List<Google.Apis.Drive.v3.Data.File> AllFiles = new List<Google.Apis.Drive.v3.Data.File>();
-            
+
             if (string.IsNullOrEmpty(TokenData) == false)
             {
                 ChangesResource.ListRequest ModifiedOnlyList = service.Changes.List(TokenData);
                 string NextPageToken = "";
                 do
                 {
-
                     var execute = ModifiedOnlyList.Execute();
                     //  ModifiedOnlyList.Fields = "nextPageToken, files(id, mimeType, name, parents, modifiedTime  )";
                     ChangesResource item;
@@ -227,12 +108,9 @@ namespace DriveQuickstart
                         {
                             AllFiles.Add(entry.File);
                         }
-
                     }
                     ModifiedOnlyList = service.Changes.List(execute.NextPageToken);
-
                 } while (string.IsNullOrEmpty(NextPageToken) == false);
-                
             }
 
             FilesResource.ListRequest listRequest = service.Files.List();
@@ -247,24 +125,20 @@ namespace DriveQuickstart
             {
                 ListAll = listRequest.Execute();
                 IList<Google.Apis.Drive.v3.Data.File> files = ListAll.Files;
-                
+
                 foreach (var file in files)
                 {
                     if (file.ModifiedTime.Value > DateTimeProgramLastRan)
                     {
-                        if (file != null) {
+                        if (file != null)
+                        {
                             AllFiles.Add(file);
                         }
                     }
                     AllExistingFiles.Add(file);
                 }
                 listRequest.PageToken = ListAll.NextPageToken;
-
             } while (string.IsNullOrEmpty(ListAll.NextPageToken) == false);
-
-            
-            
-            
 
             foreach (var file in AllFiles)
             {
@@ -272,79 +146,61 @@ namespace DriveQuickstart
 
                 if (file.MimeType.Equals(GoogleDoc) && (file?.Parents != null))
                 {
-                    Console.WriteLine("Change found in: {0}",file.Name);
+                    Console.WriteLine("Change found in: {0}", file.Name);
 
-                    string appendtofile = "_Resaved.pdf";
+                    format pdf = new format();
+                    pdf.name = "pdf";
+                    pdf.MimeHeader = "application/pdf";
 
-                    Google.Apis.Drive.v3.Data.File fileMetadata = new Google.Apis.Drive.v3.Data.File()
+                    format OpenDocument = new format();
+                    OpenDocument.name = "odt";
+                    OpenDocument.MimeHeader = "application/vnd.oasis.opendocument.text";
+
+                    format[] AllFormats = { pdf, OpenDocument };
+
+
+                    //TODO refactor into a seperate class
+                   ;
+
+                    foreach (format format in AllFormats)
                     {
-                        Name = file.Name + appendtofile,
-                        Parents = file.Parents
-                    };
-                    if (file?.Parents?.Count == null)
-                    {
-                        fileMetadata.Parents = new List<string>();
-                        fileMetadata.Parents.Add(GetOrCreateFolder(service, null));
-                    }
-                    else
-                    {
-                        fileMetadata.Parents = new List<string>();
-                        fileMetadata.Parents.Add(GetOrCreateFolder(service, file.Parents[0]));
-                    }
-                    
+                        string appendtofile = "_resaved." + format.name;
 
-                    // convert string to stream
-
-                    bool UpdateFile = true;
-
-                    
-                    
-                    foreach (var ExistingFile in AllExistingFiles)
-                    {
-                        string samplename = file.Name + appendtofile;
-
-                        if (samplename.Equals(ExistingFile.Name))
+                        Google.Apis.Drive.v3.Data.File fileMetadata = new Google.Apis.Drive.v3.Data.File()
                         {
-                            bool CanDoComparison = true;
-                            CanDoComparison = ((file?.Parents != null) );
-                            if (CanDoComparison)
-                            {
-                                string PDFID = GetOrCreateFolder(service, file.Parents[0]);
+                            Name = file.Name + appendtofile,
+                            Parents = file.Parents
+                        };
 
-                                if (ExistingFile.Parents.Contains(PDFID))
-                                {
-                                    try
-                                    {
-                                        service.Files.Delete(ExistingFile.Id).Execute();
-                                    }
-                                    catch (Exception ex)
-                                    {
-                                        Console.WriteLine("There was an error deleting: {0} with id: {1}", ExistingFile.Name, ExistingFile.Id);
-                                    }
-                                    finally
-                                    {
-                                        UpdateFile = true;
-                                    }
-                                }
-                            }
-                        } 
-                    }
-                    
-                    //Check this isn't a file that is currently open and being edited, wait till edits are made before resaving
-                    foreach (var fileRHS in AllFiles)
-                    {
-                        if (file.Id.Equals(fileRHS.Id) && file?.Parents == null)
+                        if (file?.Parents?.Count == null)
                         {
-                            Console.WriteLine(file.Name + "Is currently being modified, we will wait until changes have been completed");
-                            UpdateFile = false;
-                            break;
+                            fileMetadata.Parents = new List<string>();
+                            fileMetadata.Parents.Add(GetOrCreateFolder(service, null));
                         }
-                    }
-                    if (UpdateFile)
-                    {
-                        ResaveFile(service, file, fileMetadata, "application/pdf");
-                        fileMetadata.Name = file.Name + "_resaved.odt";
-                        ResaveFile(service, file, fileMetadata, "application/vnd.oasis.opendocument.text");
+                        else
+                        {
+                            fileMetadata.Parents = new List<string>();
+                            fileMetadata.Parents.Add(GetOrCreateFolder(service, file.Parents[0]));
+                        }
+
+                        string FileName = file.Name + appendtofile;
+                        string parent = file.Parents[0];
+
+                        Google.Apis.Drive.v3.Data.File ExistingFile = GetFilebyNameAndParent(service, parent, FileName, AllExistingFiles);
+
+                        if (ExistingFile != null)
+                        {
+                            try
+                            {
+                                service.Files.Delete(ExistingFile.Id).Execute();
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.WriteLine("There was an error deleting: {0} with id: {1}", ExistingFile.Name, ExistingFile.Id);
+                            }
+                        }
+
+                        ResaveFile(service, file, fileMetadata, format.MimeHeader);
                     }
 
                     using (System.IO.StreamWriter NewFileWrite = new System.IO.StreamWriter(LastModifiedToken, false))
@@ -357,12 +213,142 @@ namespace DriveQuickstart
                     {
                         NewFileWrite.WriteLine(DateTime.Now.Ticks);
                     }
-                   
                 }
             }
             Console.WriteLine("Finished updating");
             RanOnce = true;
             return true;
+        }
+
+        //add CanDoComparison = ((file?.Parents != null)); before running
+        private static Google.Apis.Drive.v3.Data.File GetFilebyNameAndParent(DriveService service, string parent, string filename, List<Google.Apis.Drive.v3.Data.File> ExistingFiles)
+        {
+            foreach (var ExistingFile in ExistingFiles)
+            {
+                if (filename.Equals(ExistingFile.Name))
+                {
+                    string PDFID = GetOrCreateFolder(service, parent);
+
+                    if (ExistingFile.Parents.Contains(PDFID))
+                    {
+                        return ExistingFile;
+                    }
+                }
+            }
+            return null;
+        }
+
+        private static Google.Apis.Drive.v3.Data.File GetFolderInSubfolder(DriveService driveService, string parent)
+        {
+            try
+            {
+                string pageToken = null;
+                do
+                {
+                    var request = driveService.Files.List();
+                    request.Q = "mimeType='application/vnd.google-apps.folder'";
+                    request.Spaces = "drive";
+                    request.Fields = "nextPageToken, files(id, name, parents)";
+                    request.PageToken = pageToken;
+                    var result = request.Execute();
+                    foreach (var file in result.Files)
+                    {
+                        if (string.IsNullOrEmpty(parent) | file?.Parents == null)
+                        {
+                            if (file.Name.Equals(SubfolderName))
+                            {
+                                return file;
+                            }
+                        }
+                        else
+                        {
+                            if (file.Name.Equals(SubfolderName) && file.Parents.Contains(parent))
+                            {
+                                return file;
+                            }
+                        }
+                    }
+                    pageToken = result.NextPageToken;
+                } while (pageToken != null);
+
+                return null;
+            }
+            catch
+            {
+                Console.WriteLine("Threw error");
+                return null;
+            }
+        }
+
+        private static string GetOrCreateFolder(DriveService service, string SuperFolder)
+        {
+            Google.Apis.Drive.v3.Data.File SubFolder = GetFolderInSubfolder(service, SuperFolder);
+
+            if (SubFolder?.Id != null)
+            {
+                return SubFolder.Id;
+            }
+            else
+            {
+                var FolderMetadata = new Google.Apis.Drive.v3.Data.File()
+                {
+                    Name = SubfolderName,
+                    MimeType = "application/vnd.google-apps.folder"
+                };
+                if (string.IsNullOrEmpty(SuperFolder))
+                {
+                    //do nothing
+                }
+                else
+                {
+                    FolderMetadata.Parents = new List<string>();
+                    FolderMetadata.Parents.Add(SuperFolder);
+                }
+                var request = service.Files.Create(FolderMetadata);
+                request.Fields = "id";
+
+                var folder = request.Execute();
+                Console.WriteLine("Folder ID: " + folder.Id);
+
+                return folder.Id;
+            }
+        }
+
+        private static void Main(string[] args)
+        {
+            bool WantToKeepRunning = true;
+            if (args.Length > 0)
+            {
+                WantToKeepRunning = bool.Parse(args[0]);
+            }
+            int minutes = 5;
+            if (args.Length > 1)
+            {
+                minutes = int.Parse(args[1]);
+            }
+
+            Console.WriteLine("Timer set to: " + minutes);
+
+            do
+            {
+                CheckAndUpdate();
+                Thread.Sleep(minutes * 1000 * 60);
+            } while (WantToKeepRunning | (RanOnce == false));
+        }
+
+        private static void ResaveFile(DriveService service, Google.Apis.Drive.v3.Data.File file, Google.Apis.Drive.v3.Data.File fileMetadata, string metadatatype)
+        {                        //Create New File
+            var stream = new System.IO.MemoryStream();
+            service.Files.Export(file.Id, metadatatype).Download(stream);
+            using (stream)
+            {
+                FilesResource.CreateMediaUpload request;
+                request = service.Files.Create(fileMetadata, stream, metadatatype);
+
+                request.Fields = "id";
+                request.Upload();
+                var item = request.ResponseBody;
+            }
         }
     }
 }
